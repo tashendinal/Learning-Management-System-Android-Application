5ad3d005f403bd4d8c283863b34e049c215dc7e6
Tashen Dinal <80526793+tashendinal@users.noreply.github.com>
2023-05-25 10:58:01 +0530

    Add files via upload

---
+++ windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,17 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <firebase_core/firebase_core_plugin_c_api.h>
+#include <pdfx/native_pdf_renderer_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FirebaseCorePluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FirebaseCorePluginCApi"));
+  NativePdfRendererPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("NativePdfRendererPlugin"));
+}
