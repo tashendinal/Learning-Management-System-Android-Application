7df3508a622fb054386727434edf5f2a8c1549b2
Tashen Dinal <80526793+tashendinal@users.noreply.github.com>
2025-05-24 22:35:32 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,13 +14,13 @@ Future<void> main() async {
   if (kIsWeb) {
     await Firebase.initializeApp(
       options: const FirebaseOptions(
-          apiKey: "AIzaSyAMQoK1g7YQ3tiD2xbWXOqPX1_4XHuaY58",
-  authDomain: "lms-app-97e1f.firebaseapp.com",
-  projectId: "lms-app-97e1f",
-  storageBucket: "lms-app-97e1f.firebasestorage.app",
-  messagingSenderId: "1003153251570",
-  appId: "1:1003153251570:web:1b1d0e93c1ec798641846b",
-  measurementId: "G-K1M35H6HV1"
+        apiKey: "AIzaSyAMQoK1g7YQ3tiD2xbWXOqPX1_4XHuaY58",
+        authDomain: "lms-app-97e1f.firebaseapp.com",
+        projectId: "lms-app-97e1f",
+        storageBucket: "lms-app-97e1f.appspot.com",
+        messagingSenderId: "1003153251570",
+        appId: "1:1003153251570:web:1b1d0e93c1ec798641846b",
+        measurementId: "G-K1M35H6HV1",
       ),
     );
   } else {
@@ -32,7 +32,6 @@ Future<void> main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
